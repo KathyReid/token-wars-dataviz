56d6bf96f78a6e67532e8383ecaabaaf98fbef6f
Kathy Reid <kathy@kathyreid.id.au>
2025-08-09 02:34:23 +1000

    update for GPT-5

--- visualisation.py
+++ visualisation.py
@@ -22,7 +22,8 @@ models = {
     'LLaMA 2': ('18 July 2023', 2000, 70, 'https://arxiv.org/abs/2307.09288', 'https://ai.meta.com/research/publications/llama-2-open-foundation-and-fine-tuned-chat-models/', 'GenAI, Meta'),
     'Claude 3 Opus' : ('4 March 2024', 40000, 2000, '', 'https://www.anthropic.com/news/claude-3-family', 'Anthropic'),
     'Gemma 2-27B' : ('31 July 2024',13000,27, 'https://arxiv.org/abs/2408.00118', 'https://ai.google.dev/gemma/docs/core/model_card_2', 'Google AI'),
-    'LLaMA 3-405B' : ('31 July 2024', 16550, 405, 'https://arxiv.org/abs/2407.21783', 'https://ai.meta.com/blog/meta-llama-3/', 'Meta AI')
+    'LLaMA 3-405B' : ('31 July 2024', 16550, 405, 'https://arxiv.org/abs/2407.21783', 'https://ai.meta.com/blog/meta-llama-3/', 'Meta AI'),
+    'GPT-5' : ('7 August 2025', 114000, 5000, 'https://lifearchitect.ai/gpt-5/', 'https://openai.com/index/introducing-gpt-5/', 'OpenAI')
 }
 
 # Extract data and convert dates to datetime objects
@@ -95,6 +96,12 @@ cbar.set_label('Parameters (Billions)', rotation=270, labelpad=15, fontfamily='P
 
 # Adjust axes
 plt.grid(True, linestyle='--', alpha=0.7)
+plt.axhline(y=0, color='yellow', linestyle='-', linewidth=0.5, alpha=0.6)
+
+# Axis limits 
+ax = plt.gca()
+ax.set_xlim([datetime(2018, 1, 1), datetime(2027, 1, 1)])
+ax.set_ylim([-15000, 150000])       # Instead of [-10000, 200000]      
 
 # Adjust layout to prevent label cutoff
 plt.tight_layout(pad=7)
